320a8ac8093a6046c207da756a84b2057f95475a
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-14 00:02:59 -0300

    parte 2

---
+++ Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private Vector3 fuerzaPorAplicar;   
+    private float tiempoDesdeUltimaFuerza;
+    private float intervaloTiempo; 
+    private float velocidadLateral;
+
+    void Start()
+    {
+        fuerzaPorAplicar = new Vector3(0,0,300f); 
+        tiempoDesdeUltimaFuerza = 0f;
+        intervaloTiempo = 2f; 
+        velocidadLateral = 2f;
+    }
+    private void Update()
+    {
+        float direccion = Input.GetAxis("Horizontal");
+        transform.Translate(direccion*velocidadLateral
+        *Time.deltaTime,0,0);
+    }
+
+    private void FixedUpdate()
+    {
+        tiempoDesdeUltimaFuerza += Time.fixedDeltaTime;
+        if (tiempoDesdeUltimaFuerza >= intervaloTiempo)
+        {
+            GetComponent<Rigidbody>().AddForce(fuerzaPorAplicar);
+            tiempoDesdeUltimaFuerza = 0f;
+          
+        }
+    }
+}
