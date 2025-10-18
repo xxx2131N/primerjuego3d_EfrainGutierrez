1cc4df10fec99349367c8f2c670287efd88bde03
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-18 00:33:40 -0300

    cosas por continuar

--- Assets/Prefabs/Player/Scripts/PlayerMovement.cs
+++ Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class PlayerMovement: MonoBehaviour
 {
     private Vector3 fuerzaPorAplicar;   
     private float tiempoDesdeUltimaFuerza;
