650844fc98e4d0273861ef1ed69898c569f13a87
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-18 13:22:00 -0300

    areglo

--- Assets/Prefabs/Player/Scripts/PlayerMovement.cs
+++ Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -5,20 +5,16 @@ public class PlayerMovement: MonoBehaviour
     private Vector3 fuerzaPorAplicar;   
     private float tiempoDesdeUltimaFuerza;
     private float intervaloTiempo; 
-    private float velocidadLateral;
+
 
     void Start()
     {
         fuerzaPorAplicar = new Vector3(0,0,300f); 
         tiempoDesdeUltimaFuerza = 0f;
         intervaloTiempo = 2f; 
-        velocidadLateral = 2f;
+        
     }
-    private void Update()
-    {
-        float direccion = Input.GetAxis("Horizontal");
-        transform.Translate(direccion * velocidadLateral * Time.deltaTime, 0, 0);
-    }
+
     
     private void FixedUpdate()
     {
