81563a28953394719befff3ee52dc8e2aec02b45
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-19 01:25:22 -0300

    actualizaciones por que se suvio roto

---
+++ Assets/Prefabs/Player/Patrones/IMobimientoEstrategico.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+public interface IMovimientoEstrategico
+{
+    void Move(Transform transform, float speed);
+}
--- Assets/Prefabs/Player/Scripts/PlayerMovement.cs
+++ Assets/Prefabs/Player/Scripts/PlayerMovement.cs
@@ -4,18 +4,24 @@ public class PlayerMovement: MonoBehaviour
 {
     private Vector3 fuerzaPorAplicar;   
     private float tiempoDesdeUltimaFuerza;
-    private float intervaloTiempo; 
-
+    private float intervaloTiempo;
+    private float velocidadLateral;
+    private IMovimientoEstrategico estrategia;
 
     void Start()
     {
-        fuerzaPorAplicar = new Vector3(0,0,300f); 
+        fuerzaPorAplicar = new Vector3(0, 0, 300f);
         tiempoDesdeUltimaFuerza = 0f;
-        intervaloTiempo = 2f; 
-        
+        intervaloTiempo = 2f;
+        velocidadLateral = 15f;
+        SetStrategy(new MovimientoAcelerado());
     }
 
-    
+    private void Update()
+    {
+        estrategia.Move(transform, velocidadLateral);
+    }
+
     private void FixedUpdate()
     {
         tiempoDesdeUltimaFuerza += Time.fixedDeltaTime;
@@ -23,7 +29,12 @@ public class PlayerMovement: MonoBehaviour
         {
             GetComponent<Rigidbody>().AddForce(fuerzaPorAplicar);
             tiempoDesdeUltimaFuerza = 0f;
-          
+
         }
+    }
+    
+    public void SetStrategy(IMovimientoEstrategico estrategia)
+    {
+        this.estrategia = estrategia;
     }
 }
